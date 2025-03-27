7d1c7c227601908c9d19f2da65dd71cc65bed00f
Siddhesh Abhijeet Dhonde <sd1386@rit.edu>
2025-03-27 12:05:57 -0400

    Added backend for websockets

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,31 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import {JWT_SECRET} from './config';
+import { authMiddleware } from './middleware';
 
 const app = express();
+
+app.post("/signup", (req, res) => {
+});
+
+app.post("/signin", (req, res) => {
+    const userId = req.body.userId;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        token
+    });
+});
+
+
+app.post("room", authMiddleware, (req, res) => {
+    res.json({
+        message: "Room created"
+    });
+});
+
+
 app.listen(3000);
 
--- apps/websocket-backend/src/index.ts
+++ apps/websocket-backend/src/index.ts
@@ -1,11 +1,27 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import {JWT_SECRET} from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    ws.close();
+    return;
+  }
+  const params = new URLSearchParams(url.split('?')[1]);
+  const token = params.get('token') || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
+
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    ws.send('Pong');
   });
 
-  ws.send('something');
 });
